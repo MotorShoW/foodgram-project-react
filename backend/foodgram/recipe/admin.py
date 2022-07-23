ca0cff0e925a6164593b79592a0f640babcc2bc2
Сергей <kuhtasergei@mail.ru>
2022-07-23 18:55:34 +0300

    admin.py

--- backend/foodgram/recipe/admin.py
+++ backend/foodgram/recipe/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient, Recipe, ShoppingCart, Favorite
+
+
+@admin.register(Tag)
+class AdminTag(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+
+
+@admin.register(Ingredient)
+class AdminIngredient(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+@admin.register(Recipe)
+class AdminRecipe(admin.ModelAdmin):
+    list_display = ('author', 'name', 'cooking_time')
+    list_filter = ('author', 'name')
+
+
+@admin.register(ShoppingCart)
+class AdminShoppingCart(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+
+
+@admin.register(Favorite)
+class AdminFavorite(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import Subscription
+
 User = get_user_model()
 
 
@@ -8,3 +10,9 @@ User = get_user_model()
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'username', 'email')
     list_filter = ('first_name', 'email')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    list_filter = ('user', 'author')
